42b051be70bce4507713c7de4271dd724ee1073f
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-01-21 23:30:30 +0900

    commit

---
+++ 14578_영훈이의색칠공부.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+const int MOD = 1000000007;
+
+int main() {
+    int N;          //그림의 크기
+    cin >> N;
+
+    long long result;
+    
+
+    cout << result % MOD;
+    return 0; 
+}
---
+++ 9997_폰트.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+vector<string> dict;     //단어 저장하는 사전 벡터
+vector<string> result;   //결과 문장을 저장하는 벡터
+
+bool stringCheck(string word);
+void makeString(int N, vector<string>& str, int toPick);
+
+int main() {
+    int N;      //단어의 개수(1 <= N <= 25), 한 단어의 최대 길이는 100
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        string word;
+        cin >> word;
+        dict.push_back(word);
+    }
+
+    //N개의 단어 중 M개를 선택하여 모든 조합을 찾는 재귀 함수(완전 탐색)
+    for (int M = 1; M <= N; M++)
+    {
+        vector<int> picked;
+        makeString(N, picked, M);      
+    }
+
+    cout << result.size();
+    return 0;
+}
+
+
+//N개의 단어 중 M개를 선택하여 모든 조합을 찾는 재귀 함수(완전 탐색)
+void makeString(int N, vector<int>& picked, int M) {
+
+    //조합이 완성된 문장은 temp에 임시로 담아서 알파벳 26개가 모두 있는지 검사한 후 result에 push_back()
+    if (M == 0) {
+        string temp = "";
+        for(int i = 0; i < M; i++) {
+            temp.append(dict.at(picked.at(i)));
+        }
+        
+        if (stringCheck(temp)) { result.push_back(temp); }
+        return; 
+    }
+
+    // M이 0이 될때까지 재귀 호출하며 문장 조합
+    int smallest = picked.empty() ? 0 : picked.back() + 1;
+    for (int next = smallest; next < N; next++)
+    {
+        picked.push_back(next);
+        makeString(N, picked, M - 1);
+        picked.pop_back();
+    }
+
+}
+
+
+// 알파벳이 26개 다 있는지 검사하고, 26개 다 있으면 true, 없으면 false 반환.
+bool stringCheck(string str) {
+
+    //문장의 길이가 26 미만이면 바로 리턴
+    if (str.size() < 26) { return false; }     
+
+    set<char> alphabet;
+    for (int i = 0; i < str.size(); i++) {
+        alphabet.insert(str.at(i));   
+    }
+
+    return (alphabet.size() == 26) ? true : false;
+}

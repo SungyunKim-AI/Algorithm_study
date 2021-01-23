62af040c11e991c929f9762df6142e68af16ab65
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-01-23 17:21:32 +0900

    9997

--- 9997_폰트.cpp
+++ 9997_폰트.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <string>
 using namespace std;
 
 vector<string> dict;     //단어 저장하는 사전 벡터
 vector<string> result;   //결과 문장을 저장하는 벡터
 
 bool stringCheck(string word);
-void makeString(int N, vector<string>& str, int toPick);
+void makeString(int N, vector<int>& picked, int M);
+
 
 int main() {
     int N;      //단어의 개수(1 <= N <= 25), 한 단어의 최대 길이는 100
-
     cin >> N;
     for (int i = 0; i < N; i++) {
         string word;
@@ -25,7 +26,6 @@ int main() {
         vector<int> picked;
         makeString(N, picked, M);      
     }
-
     cout << result.size();
     return 0;
 }
@@ -37,11 +37,10 @@ void makeString(int N, vector<int>& picked, int M) {
     //조합이 완성된 문장은 temp에 임시로 담아서 알파벳 26개가 모두 있는지 검사한 후 result에 push_back()
     if (M == 0) {
         string temp = "";
-        for(int i = 0; i < M; i++) {
-            temp.append(dict.at(picked.at(i)));
+        for(int i = 0; i < picked.size(); i++) {
+            temp += dict[picked[i]];
         }
-        
-        if (stringCheck(temp)) { result.push_back(temp); }
+        if (temp.size() >= 26 && stringCheck(temp)) { result.push_back(temp); }
         return; 
     }
 
@@ -58,15 +57,11 @@ void makeString(int N, vector<int>& picked, int M) {
 
 
 // 알파벳이 26개 다 있는지 검사하고, 26개 다 있으면 true, 없으면 false 반환.
-bool stringCheck(string str) {
-
-    //문장의 길이가 26 미만이면 바로 리턴
-    if (str.size() < 26) { return false; }     
-
+bool stringCheck(string str) {   
     set<char> alphabet;
     for (int i = 0; i < str.size(); i++) {
-        alphabet.insert(str.at(i));   
+        alphabet.insert(str.at(i));
+        if(alphabet.size() == 26) { return true; }   
     }
-
-    return (alphabet.size() == 26) ? true : false;
+    return false;
 }
